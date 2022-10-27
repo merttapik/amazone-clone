0d31b75ec4ecc7fc721d611c3ab83e627143011c
merttapik <merttapik@hotmail.com>
2022-10-27 23:42:41 +0300

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ConnectButton } from 'web3uikit'
+
+const Sidebar = () => {
+  return (
+    <div><ConnectButton /></div>
+  )
+}
+
+export default Sidebar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
-
+import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <MoralisProvider
+   serverUrl={process.env.REACT_APP_SERVER_URL}
+   appId={process.env.REACT_APP_APPLICATION_ID}
+   initializeOnMount={false}
+  >
+    <Component {...pageProps} />
+  </MoralisProvider>
+  
+  )
 }
 
 export default MyApp
